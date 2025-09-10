06bbff963fc5f8f6800ba4eb650d0a92a3cc0484
Elio-Benoît Sassine <2493512@edu.vaniercollege.qc.ca>
2025-09-10 10:00:49 -0400

    Init project

---
+++ src/lab2_eliosassine/Lab2_ElioSassine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2_eliosassine;
+
+/**
+ *
+ * @author 2493512
+ */
+public class Lab2_ElioSassine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
