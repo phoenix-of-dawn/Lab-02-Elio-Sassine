b2cc755a3fb6da4e143054ed0af0d4563bf62dea
Elio-Benoît Sassine <2493512@edu.vaniercollege.qc.ca>
2025-09-10 10:39:14 -0400

    done

--- src/lab2_eliosassine/Lab2_ElioSassine.java
+++ src/lab2_eliosassine/Lab2_ElioSassine.java
@@ -4,17 +4,57 @@
  */
 package lab2_eliosassine;
 
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2493512
  */
-public class Lab2_ElioSassine {
+public class Lab2_ElioSassine extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        Random rand = new Random();
+        stage.setTitle("Java Games");
+        
+        Label top = new Label("Random Game");
+        Label bot = new Label("Waiting");
+        
+        root.setTop(top);
+        root.setBottom(bot);
+        
+        String imgLink = String.format("file:images/1%02d.jpg", rand.nextInt(1, 21));
+        Image img = new Image(imgLink);
+        ImageView imgView = new ImageView(img);
+        Label imgLabel = new Label();
+        imgLabel.setGraphic(imgView);
+        
+        StackPane middle = new StackPane();
+        
+        middle.getChildren().addAll(imgLabel);
+
+        root.setCenter(middle);
+        
+        Scene scene = new Scene(root, 250, 300);
+        stage.setScene(scene);
+        
+        stage.show();
     }
     
 }
